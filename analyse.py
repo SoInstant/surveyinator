6708bed0690d3239f2e4dea7dae1fb0ac9d01e9f
Ycmelon <lohyuchen12345@gmail.com>
2019-08-01 15:18:58 +0800

    Update analyse.py

--- analyse.py
+++ analyse.py
@@ -1,1 +1,7 @@
-import openpyxl
+from openpyxl import load_workbook
+
+def analyse(file):
+
+    wb = load_workbook(filename = 'empty_book.xlsx')
+    sheet_ranges = wb['range names']
+    print(sheet_ranges['D18'].value)
