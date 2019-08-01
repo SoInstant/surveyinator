a98c97c597658dd3d87af4326c9413f9bef8d6e1
SoInstant <44108612+SoInstant@users.noreply.github.com>
2019-08-01 20:48:42 +0800

    Updated analyse.py

--- analyse.py
+++ analyse.py
@@ -1,23 +1,30 @@
 from openpyxl import load_workbook
 
 
-def open_excel(file):
+def open_excel(excel_file):
     wb = load_workbook(filename="responses.xlsx")
     ws = wb.active
     cell_values = []
     for col in ws.columns:
         col_values = [cell.value for cell in col]
         cell_values.append(col_values)
-    qn_and_response = {}
+    qn_response = {}
     for col in cell_values:
-        qn_and_response[col[0]] = col[1:]
-    return qn_and_response
+        qn_response[col[0]] = col[1:]
+    return qn_response
 
 
-def analyse(file):
+def analyse(file, config_file):
     responces = open_excel(file)
-    categorised = {"Categorical": [], "Numerical": [], "Open-ended": []}
-    return categorised
+    with open(config_file, mode="r", encoding="utf-8") as f:
+        qn_categories = [line for line in f.read().split("\n")][:-1]
+    categorised_responses = {}
+    try:
+        for pointer, items in enumerate(responces.items()):
+            categorised_responses[items[0]] = tuple([qn_categories[pointer], items[1]])
+        print(categorised_responses)
+    except IndexError:
+        # This should output to website to warn user (Fatal Error if ML doesnt work)
+        print("config_file has too little questions")
 
-
-print(analyse("responces.xlsx"))
+print(analyse("responces.xlsx","config_file.txt"))
