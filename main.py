b11cbf71f085085d335c19bbe78247004d70ef5b
Ycmelon <lohyuchen12345@gmail.com>
2019-08-02 15:38:11 +0800

    various edits

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug import secure_filename
 import os
+import analyse
+
 app = Flask('app')
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main(): #Homepage
   return render_template("upload.html", error="")
 
-@app.route("/results", methods=["GET, POST"])
+@app.route("/results", methods=["GET", "POST"])
 def analysis_page():
     if request.method == "POST":
+        if not request.files:
+            return render_template("upload.html", error="Missing file!")
         file = request.files['file']
-        if file:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     else:
         return redirect(url_for('main'))
     return "no"
-    #request.form
-    #return render_template("index.html")
+
 app.run()
