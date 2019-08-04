4d6c0b35b26244610d3555fb26365c70819c606b
SoInstant <44108612+SoInstant@users.noreply.github.com>
2019-08-04 10:34:03 +0800

    Update analyse.py
    Moved parsing functions to parse.py

--- analyse.py
+++ analyse.py
@@ -1,32 +1,6 @@
-from openpyxl import load_workbook
 from numpy import mean, median
 from scipy.stats import mode
-
-# File Parsing
-def open_excel(excel_file):
-    wb = load_workbook(excel_file)
-    ws = wb.active
-    cell_values = []
-    for col in ws.columns:
-        col_values = [cell.value for cell in col]
-        cell_values.append(col_values)
-    qn_response = {}
-    for col in cell_values:
-        qn_response[col[0]] = col[1:]
-    return qn_response
-
-
-def open_config(config_file):
-    with open(config_file, mode="r", encoding="utf-8") as f:
-        qn_categories = [line.split(" ") for line in f.read().split("\n")][:-1]
-        qn_categories = [line[1] for line in qn_categories]
-
-    # Idiot-proofing
-    allowed = ("ignore", "numerical", "categorical", "openended")
-    for category in qn_categories:
-        if category.lower() not in allowed:
-            raise ValueError("Data-type not supported")
-    return qn_categories
+from parse import open_config, open_excel
 
 
 def categorise(responses, categories):
