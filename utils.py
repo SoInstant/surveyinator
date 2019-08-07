86b72407f295ce2e22a3dab15a067f73ec9f41e4
SoInstant <44108612+SoInstant@users.noreply.github.com>
2019-08-07 21:34:37 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -68,3 +68,71 @@ def parse_config(config_file):
         if category.lower() not in allowed:
             raise ValueError("Data-type not supported")
     return tuple(qn_categories)
+
+
+def encode_str(strings):
+    import tensorflow as tf
+    import tensorflow.compat.v1 as compat
+    import tensorflow_hub as hub
+    import sentencepiece as spm
+    import numpy as np
+
+    # Load the module from TF-Hub
+    module = hub.Module("https://tfhub.dev/google/universal-sentence-encoder-lite/2")
+    input_placeholder = compat.sparse.placeholder(tf.int64, shape=[None, None])
+    encodings = module(
+        inputs=dict(
+            values=input_placeholder.values,
+            indices=input_placeholder.indices,
+            dense_shape=input_placeholder.dense_shape,
+        )
+    )
+
+    # Load SentencePiece model
+    with compat.Session() as sess:
+        spm_path = sess.run(module(signature="spm_path"))
+
+    sp = spm.SentencePieceProcessor()
+    sp.Load(spm_path)
+
+    def process_to_IDs_in_sparse_format(sp, sentences):
+        # An utility method that processes sentences with the sentence piece processor
+        # 'sp' and returns the results in tf.SparseTensor-similar format:
+        # (values, indices, dense_shape)
+        ids = [sp.EncodeAsIds(x) for x in sentences]
+        max_len = max(len(x) for x in ids)
+        dense_shape = (len(ids), max_len)
+        values = [item for sublist in ids for item in sublist]
+        indices = [
+            [row, col] for row in range(len(ids)) for col in range(len(ids[row]))
+        ]
+        return (values, indices, dense_shape)
+
+    # Compute a representation for each message, showing various lengths supported.
+    messages = strings
+
+    values, indices, dense_shape = process_to_IDs_in_sparse_format(sp, messages)
+
+    # Reduce logging output.
+    compat.logging.set_verbosity(compat.logging.ERROR)
+
+    with compat.Session() as session:
+        session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+        message_embeddings = session.run(
+            encodings,
+            feed_dict={
+                input_placeholder.values: values,
+                input_placeholder.indices: indices,
+                input_placeholder.dense_shape: dense_shape,
+            },
+        )
+
+        for i, message_embedding in enumerate(np.array(message_embeddings).tolist()):
+            print(f"Message: {messages[i]}")
+            print(f"Embedding size: {len(message_embedding)}")
+            message_embedding_snippet = ", ".join(
+                (str(x) for x in message_embedding[:3])
+            )
+            print(f"Embedding: [{message_embedding_snippet}, ...]\n")
+
+encode_str("Hi I am Junxiang")
