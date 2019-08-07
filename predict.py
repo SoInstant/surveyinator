8d8dc986ae12d718eb7f0219a466fee8007565e0
SoInstant <44108612+SoInstant@users.noreply.github.com>
2019-08-07 13:29:41 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,16 +1,16 @@
-# import parse
-# import pandas as pd
-# file = parse.parse_excel("responses.xlsx")
-# types = parse.parse_config("config_file.txt")
-# grouped = {}
-# for i, qn in enumerate(file.keys()):
-#     if types[i] not in grouped.keys():
-#         grouped[types[i]] = [qn]
-#     else:
-#         grouped[types[i]].append(qn)
-# max_len = max(len(value) for value in grouped.values())
-# for qntype, qns in grouped.items():
-#     if len(qns) != max_len:
-#         for i in range(max_len - len(qns)):
-#             grouped[qntype].append("")
-# df = pd.DataFrame(grouped)
+import utils
+import pandas as pd
+file = utils.parse_excel("responses.xlsx")
+types = utils.parse_config("config_file.txt")
+grouped = {}
+for i, qn in enumerate(file.keys()):
+    if types[i] not in grouped.keys():
+        grouped[types[i]] = [qn]
+    else:
+        grouped[types[i]].append(qn)
+max_len = max(len(value) for value in grouped.values())
+for qntype, qns in grouped.items():
+    if len(qns) != max_len:
+        for i in range(max_len - len(qns)):
+            grouped[qntype].append("")
+df = pd.DataFrame(grouped)
